72004171b2d42fbf56889a25df2d694f59c3ba61
Yasemin Ertan <yasemin.ertan@despatchcloud.com>
2021-09-18 11:45:59 +0300

    get images from unsplash

--- responsive-image-grid/src/App.vue
+++ responsive-image-grid/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ImageGrid/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageGrid from './components/ImageGrid.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImageGrid
   }
 }
 </script>
---
+++ responsive-image-grid/src/components/ImageGrid.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>Images</div>
+</template>
+
+<script>
+export default {
+  name: 'ImageGrid',
+  props: {
+  },
+  mounted(){this.getImages()},
+  methods:{
+    getImages(){
+      const getUrl='https://api.unsplash.com/photos/?client_id=iSUqmKX0n0RdggjIvapCQ39F9Pdq_DGzEL8C3YmOERA'
+      const axios = require('axios').default;
+      axios({url: getUrl, 
+          method: "GET",
+          headers: {
+              "Content-type": "application/json;charset=UTF-8",
+              'Accept': 'application/json' ,
+              'X-Requested-With': 'XMLHttpRequest',
+          }
+      })
+      .then(response =>{ 
+        console.log(response)
+      })
+      .catch(err => console.log(err));
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
